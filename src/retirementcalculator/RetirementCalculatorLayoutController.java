cecaed1e2e3a6596c7e90d59625b2b100cb8f325
Derek Barrera <derekbarrera@gmail.com>
2014-10-05 16:13:42 -0400

    Moved calculations to Retirement class

--- src/retirementcalculator/RetirementCalculatorLayoutController.java
+++ src/retirementcalculator/RetirementCalculatorLayoutController.java
@@ -1,11 +1,14 @@
 package retirementcalculator;
 
-import java.util.ArrayList;
+import java.io.File;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -13,208 +16,85 @@ import javafx.scene.control.TextField;
  * @author derekbarrera
  */
 public class RetirementCalculatorLayoutController {
-       
-        //ArrayList containing year objects
-        protected ArrayList<Year> retirement = new ArrayList<Year>();
-        protected Year year;
         
-        //TextArea Variable    
-        @FXML private TextArea calculationWindow;
-        
-        //Text input variables
-        @FXML private TextField ageField;
-        @FXML private TextField incomeTaxRateField;
-        @FXML private TextField retirementAgeField;
-        @FXML private TextField capGainsTxRateField;
-        @FXML private TextField preTaxBalanceField;
-        @FXML private TextField postTaxBalanceField;
-        @FXML private TextField preTaxContField;
-        @FXML private TextField postTaxContField;
-        @FXML private TextField rateOfReturnField;
-        @FXML private ProgressIndicator progessIndicatorObject;
-
-        @FXML protected void progess(ActionEvent event){
-        
-            double current = 0.0;
-            current += .112;
-            progessIndicatorObject.setProgress(.112);
-        
-        }
-        
-        @FXML protected void validate(ActionEvent event){
-        
-            
-        
-        }
-        
-        @FXML protected void calculate(ActionEvent event) {
-            
-           int age = getAge();
-           int retirementAge = getretirementAge();
-           int currentYear = 0;
-           int loopSize = retirementAge - age; 
-           
-            for (int i = 0; i <= loopSize; i++) {
-                
-                currentYear = i + 1;
-                
-                if(i == 0){
-                
-                    retirement.add(new Year(currentYear, getpreTaxBalanceField(), getpostTaxBalance()));
-                
-                }else if(i == loopSize){
-                    
-                    Year prevYear = getPreviousYear();
-                    double preTaxAmnt = preTaxIncomeTaxCalculation(prevYear.preTaxAmount);
-                    double postTaxAmnt = postTaxCalcuation(prevYear.postTaxAmount);
-
-                    retirement.add(new Year(currentYear, preTaxAmnt, postTaxAmnt));
-                    
-                }
-                else{   
-                    
-                    Year prevYear = getPreviousYear();
-                    double preTaxAmnt = preTaxCalculation(prevYear.preTaxAmount);
-                    double postTaxAmnt = postTaxCalcuation(prevYear.postTaxAmount);
-                    
-                    retirement.add(new Year(currentYear, preTaxAmnt, postTaxAmnt));
-                
-                }
-                
-            }
-            
-            calculationWindow.setText(toString());
-            
-        }  
-        
-        public Year getPreviousYear(){
-        
-          int arraysize = retirement.size();
-          
-          if (arraysize != 0) {
-                
-            arraysize -= 1;
-                
-          }
-
-          return retirement.get(arraysize);
-            
-        }
-        
-        public double preTaxIncomeTaxCalculation(double x){
-        
-            double preTax = x - (x * getincomeTaxRate());
-            
-            return preTax;
-        
-        }
-        
-        public double preTaxCalculation(double x){
-            
-            double preTax = x * (1 + getrateOfReturnField()) + (12 * getpreTaxContField());
-            
-            return preTax;
-            
-        }
-        
-        public double postTaxCalcuation(double x){
-                    
-            double postTax = x * (1 + getrateOfReturnField());
-            double gain = (postTax - x) * getcapGainsTxRate();
-            double answer = postTax + gain + (12 * getpostTaxContField());
-            
-            return answer;
-        
-        }
-        
-        public int getAge(){
-            
-            int input = Integer.parseInt(ageField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        public int getretirementAge(){
-            
-            int input = Integer.parseInt(retirementAgeField.getText());
-            
-            return input;
-            
-        }
-        
-        public double getincomeTaxRate(){
-            
-            double input = Double.parseDouble(incomeTaxRateField.getText());
-            
-            return input;
-            
-        }
-        
-        public double getcapGainsTxRate(){
-            
-            double input = Double.parseDouble(capGainsTxRateField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        public double getpreTaxBalanceField(){
-            
-            double input = Double.parseDouble(preTaxBalanceField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        public double getpostTaxBalance(){
-            
-            double input = Double.parseDouble(postTaxBalanceField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        public double getpreTaxContField(){
-            
-            double input = Double.parseDouble(preTaxContField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        public double getpostTaxContField(){
-            
-            double input = Double.parseDouble(postTaxContField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        public double getrateOfReturnField(){
-            
-            double input = Double.parseDouble(rateOfReturnField.getText()); //numeric string to an int'
-            
-            return input;
-            
-        }
-        
-        @Override
-        public String toString(){
-        
-            String retirementPlan = "";
-            retirementPlan = "Year" + "\t" + "PreTax Total" + "\t" + "PostTax Total" + "\t" + "Total" + "\n";
-            
-            for(int i = 0; i <= retirement.size() - 1; i++){
-            
-                Year currentYear = retirement.get(i);
-                
-                retirementPlan += currentYear;
-            
-            }
-            
-            return retirementPlan;
-        
-        }
+    //TextArea Variable
+    @FXML private TextArea calculationWindow;
     
+    //Text input variables
+    @FXML private TextField ageField, incomeTaxRateField, retirementAgeField, capGainsTxRateField, preTaxBalanceField,
+                            postTaxBalanceField, preTaxContField, postTaxContField, rateOfReturnField;
+    @FXML private ProgressIndicator progessIndicatorObject = new ProgressIndicator();
+    @FXML private LineChart<Integer,Integer> lineChart;
+    final FileChooser fileChooser = new FileChooser();
+    
+    //Dialog Box
+    Stage dialog = new Stage();
+
+    @FXML protected void calculate(ActionEvent event) {
+        
+         Retirement retirement = new Retirement(Integer.parseInt(ageField.getText()), Double.parseDouble(incomeTaxRateField.getText()), Integer.parseInt(retirementAgeField.getText()), 
+                      Double.parseDouble(capGainsTxRateField.getText()), Double.parseDouble(preTaxBalanceField.getText()), Double.parseDouble(postTaxBalanceField.getText()), Double.parseDouble(preTaxContField.getText()), 
+                      Double.parseDouble(postTaxContField.getText()), Double.parseDouble(rateOfReturnField.getText()));
+
+        calculationWindow.setText(retirement.calculate(retirement));
+    
+    }
+    
+    @FXML protected void progess(){
+        
+        double current = 0.0;
+        current += .112;
+       //progessIndicatorObject.setProgress(.112);
+        
+    }
+    
+      @FXML protected void save(ActionEvent event){
+        
+        File file = fileChooser.showOpenDialog(dialog);
+        
+        if (file != null) {
+            
+            
+            
+            
+        }
+        
+    }
+      
+          @FXML protected void exit(ActionEvent event) {
+        
+        System.exit(0);
+        
+    }
+    
+    @FXML protected void open(ActionEvent event){
+        
+        File file = fileChooser.showOpenDialog(dialog);
+       
+        if (file != null) {
+            
+            
+            
+            
+        }
+        
+    }
+    
+    public void initialize(){
+    
+        ageField.textProperty().addListener((observable, oldValue, newValue) -> {
+    
+           try{
+               ageField.getText();
+               calculationWindow.setText("GO YOU!");
+           }
+           catch(NumberFormatException e){
+               
+               calculationWindow.setText("NOPE");
+           
+           }
+    
+        });
+    
+    }
 }
